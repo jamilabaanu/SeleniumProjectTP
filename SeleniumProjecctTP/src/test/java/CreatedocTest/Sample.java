c7736a66aec24de50efa72572420573e517aecd6
Admin <Admin@DESKTOP-15REK30.mshome.net>
2025-09-26 17:53:13 +0530

    comii all

---
+++ SeleniumProjecctTP/src/test/java/CreatedocTest/Sample.java
@@ -0,0 +1,14 @@
+package CreatedocTest;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+@Test
+public void sample2()
+{System.out.println("Friday Delight1");}
+
+@Test
+public void sample1()
+{System.out.println("Friday Delight2");}
+
+}
